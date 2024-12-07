9792ece7881995cc4c4147ad7cdb4934530e5438
pouria1370 <kalantari_pouria@yahoo.com>
2024-12-07 11:01:46 +0330

    feat: integrating with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
 import "styles/tailwind.css"
-
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <header>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
